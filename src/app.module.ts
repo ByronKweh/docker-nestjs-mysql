433930e78113c8dcc5037c1f30b6890e77d3aa0a
ByronKweh <byronkweh@gmail.com>
2023-11-08 11:55:00 +0700

    fix tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { I18nModule } from './i18n/module';
 
 import { RootController } from './controllers/root.controller';
-import { ItemController } from './controllers/api/v1/item.controller';
 import { PrismaService } from './services/prisma/prisma.service';
-import { ItemService } from './services/item/item.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 
 @Module({
   imports: [I18nModule],
-  controllers: [RootController, ItemController, UserController],
-  providers: [PrismaService, ItemService, UserService],
+  controllers: [RootController, UserController],
+  providers: [PrismaService, UserService],
 })
 export class AppModule {}
